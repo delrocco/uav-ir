50124353b0290b61ca5207c791ad2bd68bb6dc7c
Joe Del Rocco <delrocco@localhost.localdomain>
2016-12-04 17:35:40 -0500

    Updated the upload script for relative path support, so it can be run from another directory. It only attempts to upload the first folder it finds that was timestamped.

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -1,27 +1,31 @@
+#!/bin/bash
 
-framesDir="../rpi_lepton/capture/frames"
+scriptdir=$(dirname $0)
+capturedir=$scriptdir"/../rpi_lepton/capture"
+framesdir=$capturedir"/frames"
 
 echo "[Timestamping frames folder]"
 if [ -d $framesDir ]
 then
-	if [ -f $(find ../rpi_lepton/capture/frames/*.pgm -maxdepth 0 -print -quit) ]
+	imgfile=$(find $framesdir"/*.pgm" -print -quit)
+	if [ -f $imgfile ]
 	then
-		timestamp=$(find ../rpi_lepton/capture/frames/*.pgm -maxdepth 0 -printf "%TY%Tm%Td_%TH%TM\n" -quit)
-		echo "Timestamping:" $framesDir "to" $framesDir"_"$timestamp
-		mv $framesDir $framesDir"_"$timestamp
+		timestamp=$(find $imgfile -printf "%TY%Tm%Td_%TH%TM\n" -quit)
+		echo "Timestamping:" $framesdir "to" $framesdir"_"$timestamp
+		mv $framesdir $framesdir"_"$timestamp
 	else
-		echo "No files matching:" $framesDir"/*.pgm"
+		echo "No files matching:" $framesdir"/*.pgm"
 	fi
 else
-	echo "Folder does not exist:" $framesDir
+	echo "Folder does not exist:" $framesdir
 fi
 
 echo "[Searching for frames_* folder]"
-if [ -d $(find ../rpi_lepton/capture/ -name "frames*" -print -quit) ]
+if [ -d $(find $capturedir -name "frames_*" -print -quit) ]
 then
-	framesDir=$(find ../rpi_lepton/capture/ -name "frames*" -print -quit)
-	echo "Uploading:" $framesDir
-	scp -r $framesDir delrocco@ps442953.dreamhost.com:/home/delrocco
+	dirfound=$(find $capturedir -name "frames_*" -print -quit)
+	echo "Uploading:" $dirfound
+	scp -r $dirfound delrocco@ps442953.dreamhost.com:/home/delrocco
 else
-	echo "Could not find anything to upload"
+	echo "Could not find timestamped folder to upload"
 fi

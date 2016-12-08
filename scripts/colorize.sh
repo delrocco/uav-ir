02e3549a3adf03251c91e787feafa49656a42ed3
Joe Del Rocco <delrocco@users.noreply.github.com>
2016-12-08 02:11:53 -0500

    This script uses ImageMagick to colorize visible grayscale files using a CLUT gradient image.

--- scripts/colorize.sh
+++ scripts/colorize.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-for f in visible/*.jpg
+mkdir -p $2
+
+for f in $1/*.jpg
 do
-	newfile="colored/"$(basename $f)
+	newfile=$2/$(basename $f)
 	convert $f \( ironbow.png -flip \) -clut $newfile
 done
